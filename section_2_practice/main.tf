9920f12e4cbd0f3feffa3cfbab405c5d1dcfcf82
David Lam <davidlam.yc@gmail.com>
2025-12-07 14:24:48 +0800

    finish up section 2

--- section_2_practice/main.tf
+++ section_2_practice/main.tf
@@ -35,5 +35,74 @@ resource "azurerm_network_interface" "webinterface01" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.websubnet01.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.webip01.id
+  }
+}
+
+resource "azurerm_public_ip" "webip01" {
+  name = "webip01"
+  resource_group_name = azurerm_resource_group.appgrp.name
+  location = local.resource_location
+  allocation_method = "Static"
+}
+
+output "websubnet01_id" {
+  value=azurerm_subnet.websubnet01.id
+}
+
+resource "azurerm_network_security_group" "app_nsg" {
+  name                = "app-nsg"
+  location            = local.resource_location
+  resource_group_name = azurerm_resource_group.appgrp.name
+
+  security_rule {
+    name                       = "AllowRDP"
+    priority                   = 300
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+// https://registry.terraform.io/providers/hashicorp/Azurerm/3.29.0/docs/resources/network_security_group
+
+resource "azurerm_subnet_network_security_group_association" "websubnet01_appnsg" {
+  subnet_id                 = azurerm_subnet.websubnet01.id
+  network_security_group_id = azurerm_network_security_group.app_nsg.id
+}
+
+// https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/subnet_network_security_group_association
+
+resource "azurerm_subnet_network_security_group_association" "appsubnet01_appnsg" {
+  subnet_id                 = azurerm_subnet.appsubnet01.id
+  network_security_group_id = azurerm_network_security_group.app_nsg.id
+}
+
+resource "azurerm_windows_virtual_machine" "webvm01" {
+  name                = var.vm_name
+  resource_group_name = azurerm_resource_group.appgrp.name
+  location            = local.resource_location
+  size                = "Standard_B2s"
+  admin_username      = var.admin_username
+  admin_password      = "Azure@123"
+  vm_agent_platform_updates_enabled = true
+  network_interface_ids = [
+    azurerm_network_interface.webinterface01.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2022-Datacenter"
+    version   = "latest"
   }
 }
---
+++ section_2_practice/variables.tf
@@ -0,0 +1,9 @@
+variable "vm_name" {
+    type = string
+    description="name of vm"
+}
+
+variable "admin_username" {
+    type = string
+    description="admin username"
+}
